2f31089036e5a2b97e56d32b36e843739280df19
Timothy Andrew <mail@timothyandrew.net>
2017-12-04 00:58:04 +0530

    Add day 2.

---
+++ src/advent_of_code_2017/day_2.clj
@@ -0,0 +1,33 @@
+(ns advent-of-code-2017.day-2)
+
+(defn diff-largest-and-smallest [row]
+  (prn row)
+  (- (apply max (remove nil? row))
+     (apply min (remove nil? row))))
+
+(defn evenly-divisible? [a b]
+  (and (not= a 0) (not= b 0)
+       (or (integer? (/ a b))
+           (integer? (/ b a)))))
+
+(defn find-evenly-divisible-pair [row]
+  (when (next row)
+    (if-let [match (first (filter #(evenly-divisible? (first row) %) (next row)))]
+      [(first row) match]
+      (find-evenly-divisible-pair (next row)))))
+
+(defn divide-pair [[a b]]
+  (if (> a b)
+    (/ a b)
+    (/ b a)))
+
+(defn run [spreadsheet]
+  (->> spreadsheet
+       (map diff-largest-and-smallest)
+       (apply +)))
+
+(defn run-2 [spreadsheet]
+  (->> spreadsheet
+       (map find-evenly-divisible-pair)
+       (map divide-pair)
+       (reduce +)))
