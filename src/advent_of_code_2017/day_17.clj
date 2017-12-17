72f880d9f13ccca281622e0025dc7e4af548cce9
Timothy Andrew <mail@timothyandrew.net>
2017-12-18 00:00:08 +0530

    day 17 part 2

--- src/advent_of_code_2017/day_17.clj
+++ src/advent_of_code_2017/day_17.clj
@@ -7,7 +7,7 @@
      insert-idx]))
 
 (defn insert [[buf current current-idx] step]
-  (let [insert-idx (rem (inc (+ current-idx step)) (count buf))]
+  (let [insert-idx (rem (inc (+ current-idx step)) (inc current))]
     [(into []
            (concat (subvec buf 0 insert-idx)
                    [(inc current)]
@@ -15,9 +15,25 @@
      (inc current)
      insert-idx]))
 
+(defn track-zero-and-adj [[[zero adj] current current-idx] step]
+  (let [current (inc current)
+        insert-idx (rem (inc (+ current-idx step)) current)
+        result (cond
+                 (< insert-idx (:index adj)) [(update zero :index inc) (update adj :index inc)]
+                 (= insert-idx (:index adj)) [zero (assoc adj :n current)]
+                 (> insert-idx (:index adj)) [zero adj])]
+    [result current insert-idx]))
+
 (defn run-1 [step rounds target]
   (let [result (reduce
                 insert
                 [[0 1] 1 1]
                 (take (dec rounds) (repeat step)))]
     (second (drop-while #(not= % target) (first result)))))
+
+(defn run-2 [step rounds]
+  (let [[[zero adj] _ _] (reduce
+                          track-zero-and-adj
+                          [[{:n 0 :index 0} {:n 1 :index 1}] 1 1]
+                          (take (dec rounds) (repeat step)))]
+    (:n adj)))
--- test/advent_of_code_2017/day_17_test.clj
+++ test/advent_of_code_2017/day_17_test.clj
@@ -7,6 +7,10 @@
   (with-open [rdr (clojure.java.io/reader "resources/day_17/long.txt")]
     (first (doall (line-seq rdr)))))
 
-(deftest runtest
+(deftest part-1
   (is (= (sut/run-1 3 2017 2017) 638))
   (is (= (sut/run-1 356 2017 2017) 808)))
+
+(deftest part-2
+  (is (= (sut/run-2 3 2017) 1226))
+  (is (= (sut/run-2 356 50000000) 47465686)))
