c51685d6e878201f082527b3721d0c2c586b6f5c
Timothy Andrew <mail@timothyandrew.net>
2017-12-26 20:10:59 +0530

    add day 21 part 1

--- scala/src/main/scala/com/advent/DayTwentyOne.scala
+++ scala/src/main/scala/com/advent/DayTwentyOne.scala
@@ -1,6 +1,28 @@
 package com.advent
 
+import com.advent.Parse.toCell
+
+case class Rule(in: Cell, out: Cell) {
+  def isMatch(cell: Cell): Boolean = {
+    cell == in || cell == in.rotateLeft || cell == in.rotateLeft.rotateLeft || cell == in.rotateLeft.rotateLeft.rotateLeft ||
+      cell == in.flip || cell == in.flip.rotateLeft || cell == in.flip.rotateLeft.rotateLeft || cell == in.flip.rotateLeft.rotateLeft.rotateLeft
+  }
+
+  def applyRule(cell: Cell): Cell = {
+    if(isMatch(cell)) out else cell
+  }
+}
+
 case class Cell(rows: Seq[Seq[Boolean]]) {
+  def flip: Cell = {
+    Cell(rows.map(_.reverse))
+  }
+
+  def transform(rules: Seq[Rule]): Cell = {
+    val rule = rules.filter(_.isMatch(this)).head
+    rule.applyRule(this)
+  }
+
   def size: Int = {
     rows.head.length
   }
@@ -12,17 +34,89 @@ case class Cell(rows: Seq[Seq[Boolean]]) {
 
     Cell(rotated)
   }
+
+  def subdivide2x2: Seq[Seq[Cell]] = {
+    rows.grouped(2).map {
+      case List(curr: Seq[Boolean], next: Seq[Boolean]) => curr.grouped(2).zip(next.grouped(2)).map {
+        case (l: Seq[Boolean], r: Seq[Boolean]) => Cell(l :: r :: Nil)
+      }.toList
+    }.toList
+  }
+
+  def subdivide3x3: Seq[Seq[Cell]] = {
+    rows.grouped(3).map {
+      case List(x: Seq[Boolean], y: Seq[Boolean], z: Seq[Boolean]) =>
+        (0 until (x.length / 3)).map { i =>
+          Cell(x.slice(i * 3, (i * 3) + 3) :: y.slice(i * 3, (i * 3) + 3) :: z.slice(i * 3, (i * 3) + 3) :: Nil)
+        }
+    }.toList
+  }
 }
 
-case class Image(rows: Seq[Seq[Cell]])
+object CombineCells {
+  def apply(cells: Seq[Seq[Cell]]): Cell = {
+    val values: Seq[Seq[Boolean]] = cells.flatMap { row: Seq[Cell] =>
+      (0 until row.head.size).map { i =>
+        row.flatMap { cell => cell.rows(i) }
+      }
+    }
+
+    Cell(values)
+  }
+}
+
+case class Image(cell: Cell, rules: Seq[Rule]) {
+  def tick: Image = {
+    val transformed: Seq[Seq[Cell]] =
+      if(cell.size % 2 == 0) {
+        cell.subdivide2x2.map { row =>
+          row.map { cell =>
+            cell.transform(rules)
+          }
+        }
+      } else {
+        cell.subdivide3x3.map { row =>
+          row.map { cell =>
+            cell.transform(rules)
+          }
+        }
+      }
+
+    val combined = CombineCells(transformed)
+    Image(combined, rules)
+  }
+
+  def raw: Seq[Boolean] = {
+    cell.rows.flatten
+  }
+
+  def tickMany(ticks: Int): Image = {
+    (0 until ticks).foldLeft[Image](this)((image: Image, _: Int) => image.tick)
+  }
+
+  def onPixels: Int = {
+    this.raw.count(_ == true)
+  }
+}
 
 object Parse {
-  def toImage(s: String): Image = {
+  def toCell(s: String): Cell = {
     val rows: Seq[Seq[Boolean]] = s.trim.split("""\/""").map { partition: String =>
       partition.split("").map(_ == "#").toList
     }.toList
 
-    Image((Cell(rows) :: Nil) :: Nil)
+    Cell(rows)
+  }
+
+  def toImage(s: String, rules: Seq[Rule]): Image = {
+    Image(toCell(s), rules)
+  }
+
+  def toRule(s: String): Rule = {
+    s.split("""=>""") match {
+      case Array(inS: String, outS: String) =>
+        Rule(toCell(inS), toCell(outS))
+    }
   }
 }
 
--- scala/src/test/scala/com/advent/DayTwentyOneSpec.scala
+++ scala/src/test/scala/com/advent/DayTwentyOneSpec.scala
@@ -2,25 +2,25 @@ package com.advent
 
 import org.scalatest.{FunSuite, Matchers}
 
-import scala.io.Source
-
 class DayTwentyOneSpec extends FunSuite with Matchers  {
-  //def long: Iterator[String] = Source.fromResource("day_21/long.txt").getLines
-
   test("Parse image") {
-    val image: Image = Parse.toImage(" ##./#../... ")
-    val cell: Cell = image.rows.head.head
+    val image: Image = Parse.toImage(" ##./#../... ", List())
+    val cell: Cell = image.cell
 
     assert(cell == Cell(List(true, true, false) :: List(true, false, false) :: List(false, false, false) :: Nil))
   }
 
-//  test("Transform enhancement rule") {
-//    val enhancementRule = Parse.toEnhancementRule("../.# => ##./#../...")
-//
-//    assert(enhancementRule.transform(Parse.toImage("#./..")) == Parse.toImage("##./#../..."))
-//    assert(enhancementRule.transform(Parse.toImage(".#/..")) == Parse.toImage("##./#../..."))
-//    assert(enhancementRule.transform(Parse.toImage("../#.")) == Parse.toImage("##./#../..."))
-//    assert(enhancementRule.transform(Parse.toImage("../.#")) == Parse.toImage("##./#../..."))
-//    assert(enhancementRule.transform(Parse.toImage("../..")) == Parse.toImage("../.."))
-//  }
+  test("Result Part 1 Short") {
+    val rules = Parse.toRule(".#./..#/### => #..#/..../..../#..#") :: Parse.toRule("../.# => ##./#../...") :: Nil
+    val image: Image = Parse.toImage(".#./..#/###", rules)
+
+    assert(image.tickMany(2).onPixels == 12)
+  }
+
+  test("Result Part 1 Long") {
+    val rules = Parse.toRule("../.. => .../.../###") :: Parse.toRule("#./.. => .../.#./.##") :: Parse.toRule("##/.. => .#./.#./...") :: Parse.toRule(".#/#. => ###/..#/.##") :: Parse.toRule("##/#. => ..#/###/#..") :: Parse.toRule("##/## => ..#/#../##.") :: Parse.toRule(".../.../... => .##./##../..##/.##.") :: Parse.toRule("#../.../... => ##../.#.#/..#./###.") :: Parse.toRule(".#./.../... => ##.#/#.#./.#../..##") :: Parse.toRule("##./.../... => ...#/##.#/.#.#/#.##") :: Parse.toRule("#.#/.../... => ..#./#.../###./...#") :: Parse.toRule("###/.../... => #.#./...#/#.#./###.") :: Parse.toRule(".#./#../... => ...#/###./.##./...#") :: Parse.toRule("##./#../... => ###./####/###./..##") :: Parse.toRule("..#/#../... => ####/#.../####/#.##") :: Parse.toRule("#.#/#../... => #.##/.#.#/##.#/###.") :: Parse.toRule(".##/#../... => ..../.#../.#.#/.##.") :: Parse.toRule("###/#../... => ..##/##.#/..##/.###") :: Parse.toRule(".../.#./... => ###./..##/.#../#..#") :: Parse.toRule("#../.#./... => ###./.#../#.../#...") :: Parse.toRule(".#./.#./... => ####/..#./.##./##..") :: Parse.toRule("##./.#./... => .#../#.#./###./###.") :: Parse.toRule("#.#/.#./... => ####/.##./##.#/.###") :: Parse.toRule("###/.#./... => #.#./..##/.##./#...") :: Parse.toRule(".#./##./... => ####/#.##/####/..#.") :: Parse.toRule("##./##./... => #.../.#../..../#.##") :: Parse.toRule("..#/##./... => #..#/..##/#.../####") :: Parse.toRule("#.#/##./... => ###./##../..##/#...") :: Parse.toRule(".##/##./... => ..../#.##/.###/#.#.") :: Parse.toRule("###/##./... => .#../##.#/.#../##..") :: Parse.toRule(".../#.#/... => ...#/.###/.##./###.") :: Parse.toRule("#../#.#/... => ###./##../#.#./.##.") :: Parse.toRule(".#./#.#/... => ..#./.#../.##./.###") :: Parse.toRule("##./#.#/... => #.../#.../.##./.#..") :: Parse.toRule("#.#/#.#/... => .##./..##/.###/#...") :: Parse.toRule("###/#.#/... => ..../####/###./....") :: Parse.toRule(".../###/... => #.##/.#.#/#.##/...#") :: Parse.toRule("#../###/... => #.../#.#./.#../#...") :: Parse.toRule(".#./###/... => ...#/###./.##./.#.#") :: Parse.toRule("##./###/... => ##../####/###./#.##") :: Parse.toRule("#.#/###/... => ...#/###./##.#/.#.#") :: Parse.toRule("###/###/... => #.#./##.#/..../.##.") :: Parse.toRule("..#/.../#.. => ...#/..#./..#./##..") :: Parse.toRule("#.#/.../#.. => ..#./#.##/#.#./#.##") :: Parse.toRule(".##/.../#.. => ####/####/#.##/#...") :: Parse.toRule("###/.../#.. => ###./..#./###./.#..") :: Parse.toRule(".##/#../#.. => ...#/####/..../###.") :: Parse.toRule("###/#../#.. => ##.#/.#../##.#/...#") :: Parse.toRule("..#/.#./#.. => ###./#.##/...#/##..") :: Parse.toRule("#.#/.#./#.. => #.../..#./..#./#.##") :: Parse.toRule(".##/.#./#.. => ##.#/...#/#.#./.###") :: Parse.toRule("###/.#./#.. => .#../..##/#.#./..#.") :: Parse.toRule(".##/##./#.. => #.../#.#./.###/#...") :: Parse.toRule("###/##./#.. => .##./.#../.#.#/.###") :: Parse.toRule("#../..#/#.. => ###./#..#/#.../##.#") :: Parse.toRule(".#./..#/#.. => #.#./#..#/#.../.###") :: Parse.toRule("##./..#/#.. => ...#/..##/..#./####") :: Parse.toRule("#.#/..#/#.. => ####/#..#/###./#.#.") :: Parse.toRule(".##/..#/#.. => ..#./..#./..../.##.") :: Parse.toRule("###/..#/#.. => ...#/#..#/#.#./....") :: Parse.toRule("#../#.#/#.. => ..##/.#.#/.###/.##.") :: Parse.toRule(".#./#.#/#.. => ..../##.#/..##/#..#") :: Parse.toRule("##./#.#/#.. => ..#./..##/#..#/#..#") :: Parse.toRule("..#/#.#/#.. => ..#./#.../#.#./##..") :: Parse.toRule("#.#/#.#/#.. => ##.#/..##/.###/...#") :: Parse.toRule(".##/#.#/#.. => #.##/.##./##../#.#.") :: Parse.toRule("###/#.#/#.. => ####/##.#/#..#/#.#.") :: Parse.toRule("#../.##/#.. => ..##/#.#./####/####") :: Parse.toRule(".#./.##/#.. => ##../###./####/....") :: Parse.toRule("##./.##/#.. => .###/####/..#./...#") :: Parse.toRule("#.#/.##/#.. => ###./##../##../#.##") :: Parse.toRule(".##/.##/#.. => ##../.###/####/.#.#") :: Parse.toRule("###/.##/#.. => ##../.##./#.../..#.") :: Parse.toRule("#../###/#.. => #.#./.#.#/#.../....") :: Parse.toRule(".#./###/#.. => .##./##../...#/##..") :: Parse.toRule("##./###/#.. => #.#./..../.##./##.#") :: Parse.toRule("..#/###/#.. => ...#/...#/##.#/...#") :: Parse.toRule("#.#/###/#.. => .##./.###/#..#/.##.") :: Parse.toRule(".##/###/#.. => ####/..##/#.../####") :: Parse.toRule("###/###/#.. => ...#/####/..#./.###") :: Parse.toRule(".#./#.#/.#. => .##./#.##/.##./.###") :: Parse.toRule("##./#.#/.#. => ..##/.#../##.#/###.") :: Parse.toRule("#.#/#.#/.#. => .#../..../.#.#/#...") :: Parse.toRule("###/#.#/.#. => ###./..#./..../#.#.") :: Parse.toRule(".#./###/.#. => #..#/.#../#.../..##") :: Parse.toRule("##./###/.#. => .##./...#/.###/....") :: Parse.toRule("#.#/###/.#. => .###/###./#.#./.#.#") :: Parse.toRule("###/###/.#. => #.##/.#.#/#.#./.##.") :: Parse.toRule("#.#/..#/##. => .###/..../####/####") :: Parse.toRule("###/..#/##. => #.##/###./..##/.##.") :: Parse.toRule(".##/#.#/##. => ..../...#/#..#/..##") :: Parse.toRule("###/#.#/##. => #.##/.#../.#../....") :: Parse.toRule("#.#/.##/##. => ..##/..##/#.../#..#") :: Parse.toRule("###/.##/##. => ##.#/#.../#.##/..##") :: Parse.toRule(".##/###/##. => ...#/..#./##../#.##") :: Parse.toRule("###/###/##. => #.##/#..#/..#./...#") :: Parse.toRule("#.#/.../#.# => ##.#/.#../##.#/.##.") :: Parse.toRule("###/.../#.# => #.#./..##/.#.#/##.#") :: Parse.toRule("###/#../#.# => ..#./#.##/...#/.###") :: Parse.toRule("#.#/.#./#.# => .###/#.##/#..#/#.##") :: Parse.toRule("###/.#./#.# => ..../..#./###./..#.") :: Parse.toRule("###/##./#.# => .###/##../..##/####") :: Parse.toRule("#.#/#.#/#.# => #.#./####/.#../.##.") :: Parse.toRule("###/#.#/#.# => ####/..../..##/#...") :: Parse.toRule("#.#/###/#.# => #.../.##./#.../...#") :: Parse.toRule("###/###/#.# => .#.#/...#/..../..##") :: Parse.toRule("###/#.#/### => .#../#.##/#.##/.###") :: Parse.toRule("###/###/### => #.../.#.#/#..#/#.##") :: Nil
+    val image: Image = Parse.toImage(".#./..#/###", rules)
+
+    assert(image.tick.tick.tick.tick.tick.onPixels == 125)
+  }
 }
